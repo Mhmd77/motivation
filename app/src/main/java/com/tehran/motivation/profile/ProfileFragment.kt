95ee87f75a67dbd3571c5163ec65be468d8d42ee
mohammad <m.tabeemam@gmail.com>
2020-11-09 18:13:18 +0330

    add profile

--- app/src/main/java/com/tehran/motivation/profile/ProfileFragment.kt
+++ app/src/main/java/com/tehran/motivation/profile/ProfileFragment.kt
@@ -7,18 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.tehran.motivation.R
+import com.tehran.motivation.databinding.FragmentProfileBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class ProfileFragment : Fragment() {
 
+    private lateinit var binding: FragmentProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
